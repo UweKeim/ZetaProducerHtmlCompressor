f507ae45a6953c05dad06d3c44cf97a61c1dfaf2
Uwe Keim <uwe.keim@gmail.com>
2015-10-19 08:07:33 +0200

    Incremented version number

--- Source/Library/Properties/AssemblyInfo.cs
+++ Source/Library/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Library")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Zeta Producer Html Compressor")]
+[assembly: AssemblyDescription("A .NET port of Google’s htmlcompressor library to minify HTML source code.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Library")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCompany("Zeta Software GmbH")]
+[assembly: AssemblyProduct("Zeta Producer Html Compressor")]
+[assembly: AssemblyCopyright("http://www.zeta-producer.com")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
