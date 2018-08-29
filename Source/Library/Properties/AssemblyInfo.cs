e2ecb4e2fa1c0a3f3e04c49061f5adf2674d3edf
Uwe Keim <uwe.keim@gmail.com>
2018-08-29 14:19:30 +0200

    Updated Version number, migrated to .NET 4.5.2 min.

--- Source/Library/Properties/AssemblyInfo.cs
+++ Source/Library/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -10,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Zeta Software GmbH")]
 [assembly: AssemblyProduct("Zeta Producer Html Compressor")]
-[assembly: AssemblyCopyright("http://www.zeta-producer.com")]
+[assembly: AssemblyCopyright("https://www.zeta-producer.com")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
--- Source/UnitTests/HtmlCompressorTests.cs
+++ Source/UnitTests/HtmlCompressorTests.cs
@@ -1,179 +1,155 @@
 ﻿namespace ZetaProducerHtmlCompressor.UnitTests
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Text.RegularExpressions;
-	using Internal;
-	using NUnit.Framework;
-	using Properties;
+    using Internal;
+    using NUnit.Framework;
+    using Properties;
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
 
-	[TestFixture]
-	public sealed class HtmlCompressorTests
-	{
-		[TestFixtureSetUp]
-		public void TestFixtureSetUp()
-		{
-			//...
-		}
+    [TestFixture]
+    public sealed class HtmlCompressorTests
+    {
+        [Test]
+        public void Test01()
+        {
+            var comp = new HtmlCompressor();
 
-		[TestFixtureTearDown]
-		public void TestFixtureTearDown()
-		{
-			//...
-		}
+            var input = Resources.Html01;
+            var output = comp.compress(input);
 
-		[SetUp]
-		public void SetUp()
-		{
-			//...
-		}
+            Assert.IsNotEmpty(output);
+            var ratio = Math.Round((double)output.Length / input.Length * 100.0, 2);
+            Assert.IsTrue(ratio < 100.0);
+        }
 
-		[TearDown]
-		public void TearDown()
-		{
-			//...
-		}
-
-		[Test]
-		public void Test01()
-		{
-			var comp = new HtmlCompressor();
-
-			var input = Resources.Html01;
-			var output = comp.compress(input);
-
-			Assert.IsNotEmpty(output);
-			var ratio = Math.Round((double)output.Length / input.Length * 100.0, 2);
-			Assert.IsTrue(ratio < 100.0);
-		}
-
-		[Test]
-		public void Test02()
-		{
-			var comp = new HtmlContentCompressor();
+        [Test]
+        public void Test02()
+        {
+            var comp = new HtmlContentCompressor();
             comp.AddPreservePattern(new Regex(@"<!-- PROTECTED: .*? -->", RegexOptions.Singleline | RegexOptions.IgnoreCase));
 
-			var input = Resources.Html02a;
-			var output = comp.Compress(input);
+            var input = Resources.Html02a;
+            var output = comp.Compress(input);
 
-			Assert.IsNotEmpty(output);
-			var ratio = Math.Round((double)output.Length / input.Length * 100.0, 2);
-			Assert.IsTrue(ratio < 100.0);
-		}
+            Assert.IsNotEmpty(output);
+            var ratio = Math.Round((double)output.Length / input.Length * 100.0, 2);
+            Assert.IsTrue(ratio < 100.0);
+        }
 
-		[Test]
-		public void testEnabled()
-		{
-			var source = Resources.testEnabled;
-			var result = Resources.testEnabledResult;
+        [Test]
+        public void testEnabled()
+        {
+            var source = Resources.testEnabled;
+            var result = Resources.testEnabledResult;
 
-			var compressor = new HtmlCompressor();
-			compressor.setEnabled(false);
+            var compressor = new HtmlCompressor();
+            compressor.setEnabled(false);
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
-		}
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
+        }
 
-		[Test]
-		public void testRemoveSpacesInsideTags()
-		{
-			var source = Resources.testRemoveSpacesInsideTags;
-			var result = Resources.testRemoveSpacesInsideTagsResult;
+        [Test]
+        public void testRemoveSpacesInsideTags()
+        {
+            var source = Resources.testRemoveSpacesInsideTags;
+            var result = Resources.testRemoveSpacesInsideTagsResult;
 
-			var compressor = new HtmlCompressor();
-			compressor.setRemoveMultiSpaces(false);
+            var compressor = new HtmlCompressor();
+            compressor.setRemoveMultiSpaces(false);
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
 
-		}
+        }
 
-		[Test]
-		public void testRemoveComments()
-		{
-			var source = Resources.testRemoveComments;
-			var result = Resources.testRemoveCommentsResult;
+        [Test]
+        public void testRemoveComments()
+        {
+            var source = Resources.testRemoveComments;
+            var result = Resources.testRemoveCommentsResult;
 
-			var compressor = new HtmlCompressor();
-			compressor.setRemoveComments(true);
-			compressor.setRemoveIntertagSpaces(true);
+            var compressor = new HtmlCompressor();
+            compressor.setRemoveComments(true);
+            compressor.setRemoveIntertagSpaces(true);
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
 
-		}
+        }
 
-		[Test]
-		public void testRemoveQuotes()
-		{
-			var source = Resources.testRemoveQuotes;
-			var result = Resources.testRemoveQuotesResult;
+        [Test]
+        public void testRemoveQuotes()
+        {
+            var source = Resources.testRemoveQuotes;
+            var result = Resources.testRemoveQuotesResult;
 
-			var compressor = new HtmlCompressor();
-			compressor.setRemoveQuotes(true);
+            var compressor = new HtmlCompressor();
+            compressor.setRemoveQuotes(true);
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
 
-		}
+        }
 
-		[Test]
-		public void testRemoveMultiSpaces()
-		{
-			var source = Resources.testRemoveMultiSpaces;
-			var result = Resources.testRemoveMultiSpacesResult;
+        [Test]
+        public void testRemoveMultiSpaces()
+        {
+            var source = Resources.testRemoveMultiSpaces;
+            var result = Resources.testRemoveMultiSpacesResult;
 
-			var compressor = new HtmlCompressor();
-			compressor.setRemoveMultiSpaces(true);
+            var compressor = new HtmlCompressor();
+            compressor.setRemoveMultiSpaces(true);
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
 
-		}
+        }
 
-		[Test]
-		public void testRemoveIntertagSpaces()
-		{
-			var source = Resources.testRemoveIntertagSpaces;
-			var result = Resources.testRemoveIntertagSpacesResult;
+        [Test]
+        public void testRemoveIntertagSpaces()
+        {
+            var source = Resources.testRemoveIntertagSpaces;
+            var result = Resources.testRemoveIntertagSpacesResult;
 
-			var compressor = new HtmlCompressor();
-			compressor.setRemoveIntertagSpaces(true);
+            var compressor = new HtmlCompressor();
+            compressor.setRemoveIntertagSpaces(true);
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
 
-		}
+        }
 
-		[Test]
-		public void testPreservePatterns()
-		{
-			var source = Resources.testPreservePatterns;
-			var result = Resources.testPreservePatternsResult;
+        [Test]
+        public void testPreservePatterns()
+        {
+            var source = Resources.testPreservePatterns;
+            var result = Resources.testPreservePatternsResult;
 
-		    var preservePatterns = new List<Regex>
-		    {
-		        HtmlCompressor.PHP_TAG_PATTERN,
-		        HtmlCompressor.SERVER_SCRIPT_TAG_PATTERN,
-		        HtmlCompressor.SERVER_SIDE_INCLUDE_PATTERN,
-		        new Regex("<jsp:.*?>", RegexOptions.Singleline | RegexOptions.IgnoreCase)
-		    };
-		    //<?php ... ?> blocks
-		    //<% ... %> blocks
-		    //<!--# ... --> blocks
-		    //<jsp: ... > tags
+            var preservePatterns = new List<Regex>
+            {
+                HtmlCompressor.PHP_TAG_PATTERN,
+                HtmlCompressor.SERVER_SCRIPT_TAG_PATTERN,
+                HtmlCompressor.SERVER_SIDE_INCLUDE_PATTERN,
+                new Regex("<jsp:.*?>", RegexOptions.Singleline | RegexOptions.IgnoreCase)
+            };
+            //<?php ... ?> blocks
+            //<% ... %> blocks
+            //<!--# ... --> blocks
+            //<jsp: ... > tags
 
-		    var compressor = new HtmlCompressor();
-			compressor.setPreservePatterns(preservePatterns);
-			compressor.setRemoveComments(true);
-			compressor.setRemoveIntertagSpaces(true);
+            var compressor = new HtmlCompressor();
+            compressor.setPreservePatterns(preservePatterns);
+            compressor.setRemoveComments(true);
+            compressor.setRemoveIntertagSpaces(true);
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
 
-		}
+        }
 
-		/*[Test]
+        /*[Test]
 		public void testCompressJavaScriptYui()
 		{
 			var source = Resources.testCompressJavaScript;
@@ -188,23 +164,23 @@
 
 		}*/
 
-		//[Test]
-		//public void testCompressJavaScriptClosure()
-		//{
-		//    var source = Resources.testCompressJavaScript;
-		//    var result = Resources.testCompressJavaScriptClosureResult;
+        //[Test]
+        //public void testCompressJavaScriptClosure()
+        //{
+        //    var source = Resources.testCompressJavaScript;
+        //    var result = Resources.testCompressJavaScriptClosureResult;
 
-		//    var compressor = new HtmlCompressor();
-		//    compressor.setCompressJavaScript(true);
-		//    compressor.setJavaScriptCompressor(new ClosureJavaScriptCompressor(CompilationLevel.ADVANCED_OPTIMIZATIONS));
-		//    compressor.setRemoveIntertagSpaces(true);
+        //    var compressor = new HtmlCompressor();
+        //    compressor.setCompressJavaScript(true);
+        //    compressor.setJavaScriptCompressor(new ClosureJavaScriptCompressor(CompilationLevel.ADVANCED_OPTIMIZATIONS));
+        //    compressor.setRemoveIntertagSpaces(true);
 
-		//    			var compress = compressor.compress(source);
-		//	Assert.AreEqual(result, compress);
+        //    			var compress = compressor.compress(source);
+        //	Assert.AreEqual(result, compress);
 
-		//}
+        //}
 
-		/*[Test]
+        /*[Test]
 		public void testCompressCss()
 		{
 			var source = Resources.testCompressCss;
@@ -219,171 +195,171 @@
 
 		}*/
 
-		[Test]
-		public void testCompress()
-		{
-			var source = Resources.testCompress;
-			var result = Resources.testCompressResult;
+        [Test]
+        public void testCompress()
+        {
+            var source = Resources.testCompress;
+            var result = Resources.testCompressResult;
 
-			var compressor = new HtmlCompressor();
+            var compressor = new HtmlCompressor();
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
-		}
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
+        }
 
-		[Test]
-		public void testSimpleDoctype()
-		{
-			var source = Resources.testSimpleDoctype;
-			var result = Resources.testSimpleDoctypeResult;
+        [Test]
+        public void testSimpleDoctype()
+        {
+            var source = Resources.testSimpleDoctype;
+            var result = Resources.testSimpleDoctypeResult;
 
-			var compressor = new HtmlCompressor();
-			compressor.setSimpleDoctype(true);
+            var compressor = new HtmlCompressor();
+            compressor.setSimpleDoctype(true);
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
 
-		}
+        }
 
-		[Test]
-		public void testRemoveScriptAttributes()
-		{
-			var source = Resources.testRemoveScriptAttributes;
-			var result = Resources.testRemoveScriptAttributesResult;
+        [Test]
+        public void testRemoveScriptAttributes()
+        {
+            var source = Resources.testRemoveScriptAttributes;
+            var result = Resources.testRemoveScriptAttributesResult;
 
-			var compressor = new HtmlCompressor();
-			compressor.setRemoveScriptAttributes(true);
+            var compressor = new HtmlCompressor();
+            compressor.setRemoveScriptAttributes(true);
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
 
-		}
+        }
 
-		[Test]
-		public void testRemoveStyleAttributes()
-		{
-			var source = Resources.testRemoveStyleAttributes;
-			var result = Resources.testRemoveStyleAttributesResult;
+        [Test]
+        public void testRemoveStyleAttributes()
+        {
+            var source = Resources.testRemoveStyleAttributes;
+            var result = Resources.testRemoveStyleAttributesResult;
 
-			var compressor = new HtmlCompressor();
-			compressor.setRemoveStyleAttributes(true);
+            var compressor = new HtmlCompressor();
+            compressor.setRemoveStyleAttributes(true);
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
 
-		}
+        }
 
-		[Test]
-		public void testRemoveLinkAttributes()
-		{
-			var source = Resources.testRemoveLinkAttributes;
-			var result = Resources.testRemoveLinkAttributesResult;
+        [Test]
+        public void testRemoveLinkAttributes()
+        {
+            var source = Resources.testRemoveLinkAttributes;
+            var result = Resources.testRemoveLinkAttributesResult;
 
-			var compressor = new HtmlCompressor();
-			compressor.setRemoveLinkAttributes(true);
+            var compressor = new HtmlCompressor();
+            compressor.setRemoveLinkAttributes(true);
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
 
-		}
+        }
 
-		[Test]
-		public void testRemoveFormAttributes()
-		{
-			var source = Resources.testRemoveFormAttributes;
-			var result = Resources.testRemoveFormAttributesResult;
+        [Test]
+        public void testRemoveFormAttributes()
+        {
+            var source = Resources.testRemoveFormAttributes;
+            var result = Resources.testRemoveFormAttributesResult;
 
-			var compressor = new HtmlCompressor();
-			compressor.setRemoveFormAttributes(true);
+            var compressor = new HtmlCompressor();
+            compressor.setRemoveFormAttributes(true);
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
 
-		}
+        }
 
-		[Test]
-		public void testRemoveInputAttributes()
-		{
-			var source = Resources.testRemoveInputAttributes;
-			var result = Resources.testRemoveInputAttributesResult;
+        [Test]
+        public void testRemoveInputAttributes()
+        {
+            var source = Resources.testRemoveInputAttributes;
+            var result = Resources.testRemoveInputAttributesResult;
 
-			var compressor = new HtmlCompressor();
-			compressor.setRemoveInputAttributes(true);
+            var compressor = new HtmlCompressor();
+            compressor.setRemoveInputAttributes(true);
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
 
-		}
+        }
 
-		[Test]
-		public void testRemoveJavaScriptProtocol()
-		{
-			var source = Resources.testRemoveJavaScriptProtocol;
-			var result = Resources.testRemoveJavaScriptProtocolResult;
+        [Test]
+        public void testRemoveJavaScriptProtocol()
+        {
+            var source = Resources.testRemoveJavaScriptProtocol;
+            var result = Resources.testRemoveJavaScriptProtocolResult;
 
-			var compressor = new HtmlCompressor();
-			compressor.setRemoveJavaScriptProtocol(true);
+            var compressor = new HtmlCompressor();
+            compressor.setRemoveJavaScriptProtocol(true);
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
 
-		}
+        }
 
-		[Test]
-		public void testRemoveHttpProtocol()
-		{
-			var source = Resources.testRemoveHttpProtocol;
-			var result = Resources.testRemoveHttpProtocolResult;
+        [Test]
+        public void testRemoveHttpProtocol()
+        {
+            var source = Resources.testRemoveHttpProtocol;
+            var result = Resources.testRemoveHttpProtocolResult;
 
-			var compressor = new HtmlCompressor();
-			compressor.setRemoveHttpProtocol(true);
+            var compressor = new HtmlCompressor();
+            compressor.setRemoveHttpProtocol(true);
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
 
-		}
+        }
 
-		[Test]
-		public void testRemoveHttpsProtocol()
-		{
-			var source = Resources.testRemoveHttpsProtocol;
-			var result = Resources.testRemoveHttpsProtocolResult;
+        [Test]
+        public void testRemoveHttpsProtocol()
+        {
+            var source = Resources.testRemoveHttpsProtocol;
+            var result = Resources.testRemoveHttpsProtocolResult;
 
-			var compressor = new HtmlCompressor();
-			compressor.setRemoveHttpsProtocol(true);
+            var compressor = new HtmlCompressor();
+            compressor.setRemoveHttpsProtocol(true);
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
 
-		}
+        }
 
-		[Test]
-		public void testPreserveLineBreaks()
-		{
-			var source = Resources.testPreserveLineBreaks;
-			var result = Resources.testPreserveLineBreaksResult;
+        [Test]
+        public void testPreserveLineBreaks()
+        {
+            var source = Resources.testPreserveLineBreaks;
+            var result = Resources.testPreserveLineBreaksResult;
 
-			var compressor = new HtmlCompressor();
-			compressor.setPreserveLineBreaks(true);
+            var compressor = new HtmlCompressor();
+            compressor.setPreserveLineBreaks(true);
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
 
-		}
+        }
 
-		[Test]
-		public void testSurroundingSpaces()
-		{
-			var source = Resources.testSurroundingSpaces;
-			var result = Resources.testSurroundingSpacesResult;
+        [Test]
+        public void testSurroundingSpaces()
+        {
+            var source = Resources.testSurroundingSpaces;
+            var result = Resources.testSurroundingSpacesResult;
 
-			var compressor = new HtmlCompressor();
-			compressor.setRemoveIntertagSpaces(true);
-			compressor.setRemoveSurroundingSpaces("p,br");
+            var compressor = new HtmlCompressor();
+            compressor.setRemoveIntertagSpaces(true);
+            compressor.setRemoveSurroundingSpaces("p,br");
 
-			var compress = compressor.compress(source);
-			Assert.AreEqual(result, compress);
+            var compress = compressor.compress(source);
+            Assert.AreEqual(result, compress);
 
-		}
-	}
+        }
+    }
 }
